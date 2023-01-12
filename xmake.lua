9cbf5dfe007346bc80870a8980d37cf46111bac9
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-01-12 14:41:03 +0800

    短小精悍

--- xmake.lua
+++ xmake.lua
@@ -1,22 +1,10 @@
 set_project("MeshOptimizer")
-
 add_rules("mode.debug", "mode.release", "mode.releasedbg", "mode.asan")
 
-option("build_shared")
-    set_default(false)
-    set_showmenu(true)
-    set_description("Toggle to build MeshOptimizer as shared library.")
-
 target("MeshOptimizer")
-    if has_config("build_shared") then 
-        set_kind("shared")
-        if (is_os("windows")) then 
-            add_defines("MESHOPTIMIZER_API=__declspec(dllimport)")
-            add_defines("MESHOPTIMIZER_API=__declspec(dllexport)")
-        else
-            add_defines("MESHOPTIMIZER_API=__attribute__((visibility(\"default\")))")
-        end
-    else
-        set_kind("static")
-    end
+    set_kind("$(kind)")
     add_files("src/**.cpp")
+    if is_kind("shared") and is_os("windows") then
+        add_defines("MESHOPTIMIZER_API=__declspec(dllexport)")
+        add_defines("MESHOPTIMIZER_API=__declspec(dllimport)", {public = true})
+    end
